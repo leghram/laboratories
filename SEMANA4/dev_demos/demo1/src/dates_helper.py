458a94f90fc6508a91c028e16f6c1dac037c9804
Miguel PL <mig.parlop@gmail.com>
2022-10-03 21:56:17 -0500

    get_dates_in_interval solved

--- SEMANA4/dev_demos/demo1/src/dates_helper.py
+++ SEMANA4/dev_demos/demo1/src/dates_helper.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+
 def get_dates_in_interval(start_date, end_date):
     result = []
     if start_date is None or end_date is None:
@@ -6,4 +9,11 @@ def get_dates_in_interval(start_date, end_date):
         return [start_date]
 
     # TODO: Solve normal use case
+    interval = timedelta(days=1)
+    range_dates = (datetime.strptime(end_date, '%m/%d/%Y') - datetime.strptime(start_date, '%m/%d/%Y')).days
+
+    for position in range(range_dates):
+        result.append(str((datetime.strptime(start_date, '%m/%d/%Y') + interval*position).date()))
+    
     return result
+

65edb748e4f625cdb29be3dd8bc0c1935c8f49e8
Miguel PL <mig.parlop@gmail.com>
2022-10-03 22:29:34 -0500

    get_default_date_data solved

--- SEMANA4/dev_demos/demo1/src/dates_helper.py
+++ SEMANA4/dev_demos/demo1/src/dates_helper.py
@@ -17,3 +17,25 @@ def get_dates_in_interval(start_date, end_date):
     
     return result
 
+
+def get_default_date_data(start_date, end_date,default_value):
+  if start_date==None or end_date == None or datetime.strptime(start_date, '%m/%d/%Y') > datetime.strptime(end_date, '%m/%d/%Y'):
+    return []
+  if datetime.strptime(start_date, '%m/%d/%Y') == datetime.strptime(end_date, '%m/%d/%Y'):
+    return [
+      {
+        'date':start_date,
+        'participants':default_value
+      }
+    ]
+  date_list = []
+  interval = timedelta(days=1)
+  range_dates = (datetime.strptime(end_date, '%m/%d/%Y') - datetime.strptime(start_date, '%m/%d/%Y')).days
+  for position in range(range_dates):
+    date_list.append(
+        {
+        'date':str((datetime.strptime(start_date, '%m/%d/%Y') + interval*position).date()),
+        'participants':default_value
+        }
+    )
+  return date_list

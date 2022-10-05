d9bc469e30c02e31c495ca709c262d22b5c3057b
Miguel PL <mig.parlop@gmail.com>
2022-10-05 13:20:22 -0500

    added to practice branch

--- PRACTICE/time-module.py
+++ PRACTICE/time-module.py
@@ -1,1 +1,16 @@
-print("this is the time module in python ")
+
+#print("this is the time module in python ")
+
+import sys
+
+option_list = ["option_1","option 2", "option 3","--help"]
+
+
+for comand in ((sys.argv)):
+  if comand in option_list[3]:
+    print(option_list)
+  else:
+    print("nothin")
+
+
+
